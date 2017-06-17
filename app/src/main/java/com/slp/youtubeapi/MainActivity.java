12ec0eb73d1ee2f2d5f1b4ea73d6bdda846049f6
Lakshmiprasad <slakshmiprasad93@gmail.com>
2017-06-17 23:26:09 +0530

    Initial commit

---
+++ app/src/main/java/com/slp/youtubeapi/MainActivity.java
@@ -0,0 +1,42 @@
+package com.slp.youtubeapi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    YouTubePlayerView youTubePlayerView;
+    Button button;
+    YouTubePlayer.OnInitializedListener onInitializedListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.play    );
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.video);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("OkgsNDqYSeU");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                youTubePlayerView.initialize(YoutubeConfig.YOUTUBE_API_KEY,onInitializedListener);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/slp/youtubeapi/YoutubeConfig.java
@@ -0,0 +1,10 @@
+package com.slp.youtubeapi;
+
+/**
+ * Created by Lakshmiprasad on 6/17/2017.
+ */
+
+public class YoutubeConfig {
+    public static final String YOUTUBE_API_KEY = "YOUR API KEY";
+
+}
